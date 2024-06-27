ae611fe85d50a110bb4123234e36e574edeaac3c
joaopombos <pv20407@alunos.estgv.ipv.pt>
2024-06-27 16:51:46 +0100

    froneende

--- frontend/src/views/Library.js
+++ frontend/src/views/Library.js
@@ -1,7 +1,7 @@
+// Library.js
 import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import axios from 'axios';
 
--- frontend/src/views/Shopsucess.js
+++ frontend/src/views/Shopsucess.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './shopsucess.css';
 
