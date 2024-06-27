e60a040bb198241f8dced736419016e1bf820185
joaopombos <pv20407@alunos.estgv.ipv.pt>
2024-06-27 21:48:59 +0100

    shop

--- frontend/src/views/Shop.js
+++ frontend/src/views/Shop.js
@@ -2,7 +2,6 @@
     import axios from 'axios';
     import 'bootstrap/dist/css/bootstrap.min.css';
     import 'bootstrap-icons/font/bootstrap-icons.css';
-    import '@fortawesome/fontawesome-free/css/all.min.css';
     import 'bootstrap/dist/js/bootstrap.bundle.min';
     
     const Shop = () => {
