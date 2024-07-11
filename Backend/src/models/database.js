a6ffcc0f105d4de7ddf288641f388dbded490f52
DrSuavidade <rafazercoconascalcas4@gmail.com>
2024-07-11 16:06:48 +0100

    ,

--- Backend/src/models/database.js
+++ Backend/src/models/database.js
@@ -2,9 +2,8 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
   database: "warebd",
-  username: "warebd_user",
-  password: "MkoQDynXsw6PcSzyF1hHhi4aBPTZWUeh",
-  host: "dpg-cpup1qqj1k6c738f3fbg-a",
+  username: "postgres",
+  password: "Atuamaede5",
   port: 5432,
   dialect: "postgres",
 });
